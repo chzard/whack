61aff29bd2ee0b833e6003c8ebcebfdde651b1eb
chzard <huang.elaine.ca@gmail.com>
2025-08-27 18:18:42 -0700

    cursor shadow + loaded test sprites

--- src/main.cpp
+++ src/main.cpp
@@ -60,20 +60,46 @@ const int SPAWN_COOLDOWN = 45;                  // Frames = 60 = 0.75 seconds
 
 const Color PLANE_LINES_COLOR = ColorFromHSV(220, 0.1, 0.6);
 const Color GRID_LINES_COLOR = ColorFromHSV(260, 0.58, 0.25);
-const Color SQUARE_COLOR = ColorFromHSV(124, 0.63, 0.41);
 const Color LIVES_COLOR = ColorFromHSV(359, 0.75, 0.8);
+const Color CURSOR_SHADOW = {82,83,107,90};
+
+const int INIT_SQ_HUE = 125;
+const double INIT_SQ_SAT = 0.70;
+const double INIT_SQ_VAL = 0.70;
+
+const int INIT_WRSQ_HUE = 5;
+const double INIT_WRSQ_SAT = 0.70;
+const double INIT_WRSQ_VAL = 0.85;
+
+const Color SQUARE_COLOR = ColorFromHSV(INIT_SQ_HUE, INIT_SQ_SAT, INIT_SQ_VAL);
+const Color SQUARE_WR_COLOR = ColorFromHSV(INIT_WRSQ_HUE, INIT_WRSQ_SAT, INIT_WRSQ_VAL);
+
+// LOAD IMAGING
+
+//const Image TEST_SPRITE_GREEN = LoadImage("../assets/sprites/TestSpriteGreen_scaled.png");
 
 //GLOBAL
 
 int lives = 5;
 int score = 0;
+
+bool gameStart = false;
 bool gameOver = false;
 
-int framesLitTime[SQUARESY][SQUARESX];          // Stores by row x column
 bool framesLit[SQUARESY][SQUARESX];             // Stores by row x column
+bool framesWrLit[SQUARESY][SQUARESX];           // Stores by row x column
+int framesLitTime[SQUARESY][SQUARESX];          // Stores by row x column
 int framesCooldown[SQUARESY][SQUARESX];        // Stores by row x column
+
+//CONDENSED
+int framesAction[SQUARESY][SQUARESX];           // 0 = None, 1 = Incorrectly Clicked, 2 = Lit, 3 = 
+int framesSprite[SQUARESY][SQUARESX];           // 0 = None, 1 = Baby, 2 = Pitcher, 3 = Vase
+int framesActionTime[SQUARESY][SQUARESY];
+
 int framesLitNum = 0;
 int squaresSpawnCooldown = 0;
+
+// COLOR CHANGES: OPTIONAL
 
 Vector2 mousePos = {0,0};
 
@@ -81,22 +107,31 @@ Vector2 mousePos = {0,0};
 
 void initGameScreen(void);
 void updateGame(void);
+void updateFrame(void);
+
 void drawPlane(void);
 void drawGrid(void);
 void drawLives(void);
 void drawSquares(void);
+void drawCursorShadow(void);
+//void drawGameStartScreen(void);
 void drawGameScreen(void);
 void drawEndScreen(void);
-void updateFrame(void);
 void displayStats(void);
+
 void spawnSquare(void);
-void resetSquare(int x, int y);
+void resetSquare(int x, int y, bool wr);
+
+// bool isSquareInactive(int x, int y);
+// bool isSquareLit(int x, int y);
+// bool isSquareWrLit(int x, int y);
+// bool isSquareFade(int x, int y);
+// bool isSquareCooldown(int x, int y);
 
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
-int main(void)
-{
+int main(void) {
     // Initialization
     //--------------------------------------------------------------------------------------
 
@@ -118,7 +153,7 @@ int main(void)
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
-
+            if (!gameStart) {}
             ClearBackground(RAYWHITE);
             updateFrame();
 
@@ -141,6 +176,7 @@ void initGameScreen(void) {
             framesLitTime[i][j]=0;
             framesCooldown[i][j]=0;
             framesLit[i][j]=false;
+            framesWrLit[i][j]=false;
             framesLitNum=0;
         }
     } 
@@ -149,9 +185,9 @@ void initGameScreen(void) {
 }
 
 void updateGame(void) {
+    mousePos = GetMousePosition();
     if (squaresSpawnCooldown) {squaresSpawnCooldown--;}
     if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-        mousePos = GetMousePosition();
         // If mouse is in the grid region
         if (mousePos.x >= GRID_MARGIN_LEFT && 
             mousePos.x <= SCREEN_WIDTH-GRID_MARGIN_RIGHT && 
@@ -161,21 +197,35 @@ void updateGame(void) {
             int sqX = (mousePos.y - GRID_MARGIN_TOP) / SQUARE_SIZE;
             int sqY = (mousePos.x - GRID_MARGIN_BOTTOM) / SQUARE_SIZE;   
             
-            if (framesLit[sqX][sqY]) {resetSquare(sqX, sqY);}
+            //if (isSquareLit(sqX, sqY)) {resetSquare(sqX, sqY, false);}
+            if (framesLit[sqX][sqY]) {resetSquare(sqX,sqY,false);}
+            else {
+                framesWrLit[sqX][sqY] = true;
+                lives--;
+                if (lives == 0) {
+                    gameOver = true;
+                    return;
+                }
+            }
         }
     }
 
     for (int i=0; i<SQUARESY; i++) {
         for (int j=0; j<SQUARESX; j++) {
             if (framesCooldown[i][j]) {framesCooldown[i][j]--;}
-            if (framesLit[i][j]) {framesLitTime[i][j]++;}
-            if (framesLitTime[i][j] > LIT_TIME){
-                lives--;
-                if (lives == 0) {
-                    gameOver = true;
-                    return;
+            if (framesLit[i][j] || framesWrLit[i][j]) {
+                framesLitTime[i][j]++;
+                if (framesLit[i][j] && framesLitTime[i][j] > LIT_TIME){
+                    lives--;
+                    if (lives == 0) {
+                        gameOver = true;
+                        return;
+                    }
+                    resetSquare(i,j,false);
                 }
-                resetSquare(i,j);
+                if (framesWrLit[i][j] && framesLitTime[i][j] > LIT_TIME) {
+                    resetSquare(i,j,true);
+                }
             }
         }
     }
@@ -191,9 +241,10 @@ void updateGame(void) {
 
 void drawGameScreen(void) {
     drawPlane();
-    drawGrid();
     drawSquares();
     drawLives();
+    drawGrid();
+    drawCursorShadow();
     // displayStats();
 }
 
@@ -203,7 +254,7 @@ void drawEndScreen(void) {
 }
 
 void updateFrame(void) {
-    if (gameOver) {drawEndScreen(); return;}
+    if (gameOver) {drawEndScreen(); drawCursorShadow(); return;}
     updateGame();
     drawGameScreen();
 }
@@ -241,12 +292,12 @@ void drawLives(void) {
 void drawSquares(void) {
     for (int i=0; i<SQUARESY; i++) {
         for (int j=0; j<SQUARESX; j++) {
-            if (framesLit[i][j]) {
+            if (framesLit[i][j] || framesWrLit[i][j]) {
                 //Draw square
                 DrawRectangleV(
                     (Vector2){GRID_MARGIN_LEFT + (j * SQUARE_SIZE), GRID_MARGIN_TOP + (i*SQUARE_SIZE)},
                     (Vector2){SQUARE_SIZE, SQUARE_SIZE},
-                    SQUARE_COLOR
+                    (framesLit[i][j] ? SQUARE_COLOR : SQUARE_WR_COLOR)
                 );
             }
         }
@@ -263,13 +314,17 @@ void displayStats(void) {
     }
 }
 
+void drawCursorShadow(void) {
+    DrawCircle(mousePos.x, mousePos.y, (SQUARE_SIZE/2)+10, CURSOR_SHADOW);
+}
+
 // ---------------------------------
 // UPDATE HELPERS
 
 void spawnSquare(void) {
     while (true) {
         int ran = GetRandomValue(0,(SQUARESX*SQUARESY)-1);
-        if (!framesLit[(int)ran/6][ran%6] && !framesCooldown[(int)ran/6][ran%6]) {
+        if (!framesLit[(int)ran/6][ran%6] && !framesWrLit[(int)ran/6][ran%6] && !framesCooldown[(int)ran/6][ran%6]) {
             framesLit[(int)ran/6][ran%6] = true;
             framesLitNum++;
             return;
@@ -277,9 +332,30 @@ void spawnSquare(void) {
     }
 }
 
-void resetSquare(int x, int y) {
+void resetSquare(int x, int y, bool wr) {
     framesLit[x][y] = false;
+    framesWrLit[x][y] = false;
     framesLitTime[x][y] = 0;
     framesCooldown[x][y] = COOLDOWN;
-    framesLitNum--;
+    if (!wr) {framesLitNum--;}
+}
+
+bool isSquareInactive(int x, int y) {
+    return (framesAction[x][y]==0);
+}
+
+bool isSquareLit(int x, int y) {
+    return (framesAction[x][y]==1) && (framesActionTime[x][y]>0);
+}
+
+bool isSquareWrLit(int x, int y) {
+    return (framesAction[x][y]==2) && (framesActionTime[x][y]>0);
+}
+
+bool isSquareFade(int x, int y) {
+    return (framesAction[x][y]==3) && (framesActionTime[x][y]>0);
+}
+
+bool isSquareCooldown(int x, int y) {
+    return (framesAction[x][y]==4) && (framesActionTime[x][y]>0);
 }
