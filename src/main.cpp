cc038a0574714b23606d39b2c264138e8c2d9f53
chzard <huang.elaine.ca@gmail.com>
2025-08-28 19:52:00 -0700

    Added 'standby' squares, cleaned up code, updated gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,22 @@
 /*******************************************************************************************
-*
-*   raylib [core] example - Basic window
-*
-*   Example complexity rating: [★☆☆☆] 1/4
-*
-*   Welcome to raylib!
-*
-*   To test examples, just press F6 and execute 'raylib_compile_execute' script
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   To test the examples on Web, press F6 and execute 'raylib_compile_execute_web' script
-*   Web version of the program is generated in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2013-2025 Ramon Santamaria (@raysan5)
+*  whac-a-mole, but slightly different :)
+*  @author: chzard8 (on github)
 *
 ********************************************************************************************/
 
 #include "../include/raylib.h"
+#include <string>
 #include <algorithm>
-#include <format>
 using namespace std;
 
 //DEFINES
+
+// Corresponding actions & nums
+#define NO_ACTION 0
+#define LIT 1
+#define INC_CLICK 2
+#define STANDBY 3
+#define COOL 4
 
 //GLOBAL VARIABLES
 
@@ -53,26 +38,19 @@ const int SQUARESX = GRID_WIDTH / SQUARE_SIZE;  // Num of squares per row
 const int SQUARESY = GRID_HEIGHT / SQUARE_SIZE; // Num of squares per column
 
 const int LIT_LIMIT = 5;                        // Max amount of lit squares at any frame
-const int SPAWN_MAX = 3;                        // Max amount of squares that can spawn after cooldown
+const int STANDBY_LIMIT = 8;
+const int SPAWN_MAX = 4;                        // Max amount of squares that can spawn after cooldown
 const int LIT_TIME = 150;                       // Frames = 150 = 2.5 seconds 
 const int COOLDOWN = 180;                       // Frames = 180 = 3 seconds
-const int SPAWN_COOLDOWN = 45;                  // Frames = 60 = 0.75 seconds
+const int SPAWN_COOLDOWN = 30;                  // Frames = 30 = 0.5 seconds
 
 const Color PLANE_LINES_COLOR = ColorFromHSV(220, 0.1, 0.6);
 const Color GRID_LINES_COLOR = ColorFromHSV(260, 0.58, 0.25);
 const Color LIVES_COLOR = ColorFromHSV(359, 0.75, 0.8);
 const Color CURSOR_SHADOW = {82,83,107,90};
-
-const int INIT_SQ_HUE = 125;
-const double INIT_SQ_SAT = 0.70;
-const double INIT_SQ_VAL = 0.70;
-
-const int INIT_WRSQ_HUE = 5;
-const double INIT_WRSQ_SAT = 0.70;
-const double INIT_WRSQ_VAL = 0.85;
-
-const Color SQUARE_COLOR = ColorFromHSV(INIT_SQ_HUE, INIT_SQ_SAT, INIT_SQ_VAL);
-const Color SQUARE_WR_COLOR = ColorFromHSV(INIT_WRSQ_HUE, INIT_WRSQ_SAT, INIT_WRSQ_VAL);
+const Color SQUARE_COLOR = ColorFromHSV(125, 0.7, 0.7);
+const Color SQUARE_WR_COLOR = ColorFromHSV(5, 0.7, 0.85);
+const Color SQUARE_SB_COLOR = ColorFromHSV(45, 0.78, 0.85);
 
 // LOAD IMAGING
 
@@ -83,23 +61,15 @@ const Color SQUARE_WR_COLOR = ColorFromHSV(INIT_WRSQ_HUE, INIT_WRSQ_SAT, INIT_WR
 int lives = 5;
 int score = 0;
 
-bool gameStart = false;
+bool gameStart = true;
 bool gameOver = false;
 
-bool framesLit[SQUARESY][SQUARESX];             // Stores by row x column
-bool framesWrLit[SQUARESY][SQUARESX];           // Stores by row x column
-int framesLitTime[SQUARESY][SQUARESX];          // Stores by row x column
-int framesCooldown[SQUARESY][SQUARESX];        // Stores by row x column
-
-//CONDENSED
-int framesAction[SQUARESY][SQUARESX];           // 0 = None, 1 = Incorrectly Clicked, 2 = Lit, 3 = 
-int framesSprite[SQUARESY][SQUARESX];           // 0 = None, 1 = Baby, 2 = Pitcher, 3 = Vase
-int framesActionTime[SQUARESY][SQUARESY];
-
 int framesLitNum = 0;
+int framesStandbyNum = 0;
 int squaresSpawnCooldown = 0;
 
 // COLOR CHANGES: OPTIONAL
+
 
 Vector2 mousePos = {0,0};
 
@@ -114,19 +84,18 @@ void drawGrid(void);
 void drawLives(void);
 void drawSquares(void);
 void drawCursorShadow(void);
-//void drawGameStartScreen(void);
 void drawGameScreen(void);
 void drawEndScreen(void);
-void displayStats(void);
 
-void spawnSquare(void);
-void resetSquare(int x, int y, bool wr);
+void spawnSquare(int act);
+void resetSquare(int x, int y, int act);
+void RESET_ALL(void);
 
-// bool isSquareInactive(int x, int y);
-// bool isSquareLit(int x, int y);
-// bool isSquareWrLit(int x, int y);
-// bool isSquareFade(int x, int y);
-// bool isSquareCooldown(int x, int y);
+struct {
+    int action;
+    int actionTime;
+    string sprite;
+} squares[SQUARESY][SQUARESX];
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -135,12 +104,13 @@ int main(void) {
     // Initialization
     //--------------------------------------------------------------------------------------
 
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "whac-a-what?");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "im gonna fucking kill myself");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
     initGameScreen();
+
     // INITIALIZE MUSIC
     InitAudioDevice();
     Music TEST_BGM = LoadMusicStream("../assets/bgm/Test_song.mp3");
@@ -160,7 +130,6 @@ int main(void) {
         UpdateMusicStream(TEST_BGM);
 
         BeginDrawing();
-            if (!gameStart) {}
             ClearBackground(RAYWHITE);
             updateFrame();
 
@@ -180,23 +149,18 @@ int main(void) {
 }
 
 void initGameScreen(void) {
+    RESET_ALL();
 
-    for (int i=0; i<SQUARESY; i++) {
-        for (int j=0; j<SQUARESX; j++) {
-            framesLitTime[i][j]=0;
-            framesCooldown[i][j]=0;
-            framesLit[i][j]=false;
-            framesWrLit[i][j]=false;
-            framesLitNum=0;
-        }
-    } 
-    //INITIALIZE: SPAWN MAX AMOUNT OF SQUARES
-    for (int i=0; i<LIT_LIMIT; i++) {spawnSquare();}
-
+    // PSA: if you see a cs major, pet it (it's probably in desperate need of petting)
+    // watch house md you freaky little herpeee
+        
+    //INITIALIZE: SPAWN MAX AMOUNT OF LIT SQUARES
+    for (int i=0; i<LIT_LIMIT; i++) {spawnSquare(LIT);}
 }
 
 void updateGame(void) {
     if (squaresSpawnCooldown) {squaresSpawnCooldown--;}
+
     if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
         // If mouse is in the grid region
         if (mousePos.x >= GRID_MARGIN_LEFT && 
@@ -204,13 +168,12 @@ void updateGame(void) {
             mousePos.y >= GRID_MARGIN_TOP && 
             mousePos.y <= SCREEN_HEIGHT-GRID_MARGIN_BOTTOM) {
             
-            int sqX = (mousePos.y - GRID_MARGIN_TOP) / SQUARE_SIZE;
-            int sqY = (mousePos.x - GRID_MARGIN_BOTTOM) / SQUARE_SIZE;   
-            
-            //if (isSquareLit(sqX, sqY)) {resetSquare(sqX, sqY, false);}
-            if (framesLit[sqX][sqY]) {resetSquare(sqX,sqY,false);}
+            int sqX = (mousePos.x - GRID_MARGIN_LEFT) / SQUARE_SIZE;   
+            int sqY = (mousePos.y - GRID_MARGIN_TOP) / SQUARE_SIZE;
+
+            if (squares[sqY][sqX].action == LIT) {resetSquare(sqX, sqY, LIT);}
             else {
-                framesWrLit[sqX][sqY] = true;
+                squares[sqY][sqX].action = INC_CLICK;
                 lives--;
                 if (lives == 0) {
                     gameOver = true;
@@ -222,28 +185,34 @@ void updateGame(void) {
 
     for (int i=0; i<SQUARESY; i++) {
         for (int j=0; j<SQUARESX; j++) {
-            if (framesCooldown[i][j]) {framesCooldown[i][j]--;}
-            if (framesLit[i][j] || framesWrLit[i][j]) {
-                framesLitTime[i][j]++;
-                if (framesLit[i][j] && framesLitTime[i][j] > LIT_TIME){
-                    lives--;
-                    if (lives == 0) {
-                        gameOver = true;
-                        return;
+            if (squares[i][j].action == NO_ACTION) {continue;}
+            if (squares[i][j].action == COOL) {
+                if (squares[i][j].actionTime == 0) {squares[i][j].action = NO_ACTION;}
+                else {squares[i][j].actionTime--;}
+            }
+            else {
+                squares[i][j].actionTime++;
+                if (squares[i][j].actionTime > LIT_TIME){
+                    resetSquare(i, j, squares[i][j].action);
+                    if (squares[i][j].action == LIT) {
+                        lives--;
+                        if (lives == 0) {
+                            gameOver = true;
+                            return;
+                        }
                     }
-                    resetSquare(i,j,false);
-                }
-                if (framesWrLit[i][j] && framesLitTime[i][j] > LIT_TIME) {
-                    resetSquare(i,j,true);
+                    squares[i][j].action = COOL;
+                    squares[i][j].actionTime = COOLDOWN;
                 }
             }
         }
     }
 
     if (!squaresSpawnCooldown) {
-        for (int i=0; i<min(SPAWN_MAX, LIT_LIMIT-framesLitNum); i++) { 
-            spawnSquare();
-        }
+        int LS = min(GetRandomValue(0,SPAWN_MAX), LIT_LIMIT - framesLitNum); 
+        int SBS = min(SPAWN_MAX - LS, STANDBY_LIMIT - framesStandbyNum);
+        for (int i=0; i<LS; i++) {spawnSquare(LIT);}
+        for (int i=0; i<SBS; i++) {spawnSquare(STANDBY);}
         squaresSpawnCooldown = SPAWN_COOLDOWN;
     }
 
@@ -255,7 +224,6 @@ void drawGameScreen(void) {
     drawLives();
     drawGrid();
     drawCursorShadow();
-    // displayStats();
 }
 
 void drawEndScreen(void) {
@@ -297,76 +265,62 @@ void drawLives(void) {
     for (int i=0; i<lives; i++) {
         DrawCircle(GRID_MARGIN_LEFT + (GRID_WIDTH / (2 * lives)) + (i * GRID_WIDTH / lives), GRID_MARGIN_TOP/2, 50, LIVES_COLOR);
     }
-    return;
 }
 
 void drawSquares(void) {
     for (int i=0; i<SQUARESY; i++) {
         for (int j=0; j<SQUARESX; j++) {
-            if (framesLit[i][j] || framesWrLit[i][j]) {
+            if (squares[i][j].action != NO_ACTION && squares[i][j].action != COOL) {
                 //Draw square
                 DrawRectangleV(
                     (Vector2){GRID_MARGIN_LEFT + (j * SQUARE_SIZE), GRID_MARGIN_TOP + (i*SQUARE_SIZE)},
                     (Vector2){SQUARE_SIZE, SQUARE_SIZE},
-                    (framesLit[i][j] ? SQUARE_COLOR : SQUARE_WR_COLOR)
+                    (squares[i][j].action == LIT ? 
+                        SQUARE_COLOR : 
+                        (squares[i][j].action == INC_CLICK ? 
+                            SQUARE_WR_COLOR : 
+                            SQUARE_SB_COLOR)
+                    )
                 );
             }
         }
     }
 }
 
-void displayStats(void) {
-    DrawText(TextFormat("FRAMES LIT NUM: %d", framesLitNum), 50, 25, 10, PLANE_LINES_COLOR);
-    DrawText(TextFormat("SQUARE SPAWN COOLDOWN: %d", squaresSpawnCooldown), 200, 25, 10, PLANE_LINES_COLOR);
-    for (int i=0; i<SQUARESY; i++) {
-        for (int j=0; j<SQUARESX; j++) {
-            DrawText(TextFormat("(L: %d, LT: %d, CD: %d)", framesLit[i][j], framesLitTime[i][j], framesCooldown[i][j]), 20 + (j * 120), 50 + (i * 50), 10, GetColor(0x0b0d40));
-        }
-    }
-}
-
 void drawCursorShadow(void) {
-    DrawCircle(mousePos.x, mousePos.y, (SQUARE_SIZE/2)+10, CURSOR_SHADOW);
+    DrawCircle(mousePos.x, mousePos.y, (SQUARE_SIZE/3)+10, CURSOR_SHADOW);
 }
 
 // ---------------------------------
 // UPDATE HELPERS
 
-void spawnSquare(void) {
-    while (true) {
+void spawnSquare(int act) {
+    for (int i=0; i<(SQUARESX * SQUARESY); i++) {
         int ran = GetRandomValue(0,(SQUARESX*SQUARESY)-1);
-        if (!framesLit[(int)ran/6][ran%6] && !framesWrLit[(int)ran/6][ran%6] && !framesCooldown[(int)ran/6][ran%6]) {
-            framesLit[(int)ran/6][ran%6] = true;
-            framesLitNum++;
+        if (squares[(int)ran/6][ran%6].action == 0) {
+            squares[(int)ran/6][ran%6].action = act;
+            if (act == LIT) {framesLitNum++;}
+            if (act == STANDBY) {framesStandbyNum++;}
             return;
         } 
     }
 }
 
-void resetSquare(int x, int y, bool wr) {
-    framesLit[x][y] = false;
-    framesWrLit[x][y] = false;
-    framesLitTime[x][y] = 0;
-    framesCooldown[x][y] = COOLDOWN;
-    if (!wr) {framesLitNum--;}
+void resetSquare(int x, int y, int act) {
+    squares[y][x].action = NO_ACTION;
+    squares[y][x].actionTime = 0;
+    if (act == LIT) {framesLitNum--;}
+    else if (act == STANDBY) {framesStandbyNum--;}
 }
 
-bool isSquareInactive(int x, int y) {
-    return (framesAction[x][y]==0);
-}
-
-bool isSquareLit(int x, int y) {
-    return (framesAction[x][y]==1) && (framesActionTime[x][y]>0);
-}
-
-bool isSquareWrLit(int x, int y) {
-    return (framesAction[x][y]==2) && (framesActionTime[x][y]>0);
-}
-
-bool isSquareFade(int x, int y) {
-    return (framesAction[x][y]==3) && (framesActionTime[x][y]>0);
-}
-
-bool isSquareCooldown(int x, int y) {
-    return (framesAction[x][y]==4) && (framesActionTime[x][y]>0);
+void RESET_ALL() {
+    for (int i=0; i<SQUARESY; i++) {
+        for (int j=0; j<SQUARESX; j++) {
+            squares[i][j].action = NO_ACTION;
+            squares[i][j].actionTime = 0;
+            squares[i][j].sprite = "NONE";
+        }
+    }
+    framesLitNum=0;
+    framesStandbyNum=0;
 }
